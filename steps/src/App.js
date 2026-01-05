44a999685727b4412878c3133830a1b585a58ae4
Pardip1998 <52111728+Pardip1998@users.noreply.github.com>
2026-01-04 22:17:33 -0800

    steps project upload

---
+++ steps/src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handledlePrevious() {
+    if (step === 1) return;
+    setStep((s) => s - 1);
+  }
+
+  function handledleNext() {
+    if (step === messages.length) return;
+    setStep((s) => s + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((io) => !io)}>
+        &times;
+      </button>
+      <div hidden={isOpen} className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+
+        <p className="message">
+          Step: {step} {messages[step - 1]}
+        </p>
+
+        <div className="buttons">
+          <button
+            onClick={handledlePrevious}
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          >
+            Previous
+          </button>
+          <button
+            onClick={handledleNext}
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
